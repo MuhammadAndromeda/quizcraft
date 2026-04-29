7cd67c293bb019b9d4f0716fab45ef5160f00e50
MuhammadAndromeda <q.a.r.h.hrp0204@gmail.com>
2026-04-28 21:12:12 -0700

    Add Logo Icon

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ function App() {
   return(
     <main className="app-container w-full h-max py-10 flex flex-col justify-start items-center">
       <header className='w-full h-auto mb-4 flex flex-col justify-center items-center'>
+        <img 
+          src="/logo.svg" 
+          alt="QuizCraft AI Logo" 
+          className="w-30 h-auto"
+        />
         <p className='text-white text-7xl text-center font-bold capitalize leading-snug'>QuizCraft AI</p>
         <p className='text-gray-400 text-base text-center font-medium capitalize'>Ketik topik apapun, langsung dapat soal latihan!</p>
       </header>
